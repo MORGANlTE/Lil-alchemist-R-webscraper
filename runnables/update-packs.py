ecbbc9d8fcb7f52f296510f7847cff7e1011aaa6
Laurens Arnauts <laurens.arnauts@student.hogent.be>
2023-12-03 22:51:57 +0100

    pack placeholder replaced

--- runnables/update-packs.py
+++ runnables/update-packs.py
@@ -3,16 +3,7 @@ from essentials.scraper import overwrite_packs
 # add a card to the cards_to_scrape list to scrape it
 # if it's an onyx aswell, add it to the onyx_cards_to_scrape list
 
-packs_to_scrape = [
-    "Golem",
-    "Turkey Day",
-    "Carver of Doom",
-    "Harvest",
-    "Plymouth Rock",
-    "Robot Turkey",
-    "Turkey Zombie",
-    "Arcane Golem",
-]
+packs_to_scrape = [""]
 
 
 overwrite_packs(packs_to_scrape)
